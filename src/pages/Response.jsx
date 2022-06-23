a7425d366e4035ae5cbce5c547659cb766fb4e12
KristinaMesyachik <mesyachik2001@gmail.com>
2022-06-23 14:35:06 +0300

    Update page responses

--- src/pages/Response.jsx
+++ src/pages/Response.jsx
@@ -1,83 +1,77 @@
 import React, { Component } from 'react'
 import FieldService from "../services/FieldService";
 import SockJsClient from 'react-stomp';
+import ResponseService from "../services/ResponseService";
+import cl2 from '../components/styles/ListField.css'
+
+function Find(props) {
+  let qqq = (props.answers).filter((ans) => (
+    ans.field.id === props.fields.id))
+  if (qqq.length === 0) {
+    return (<td>N/A</td>)
+  } else {
+    return (<td>{qqq.map(qqq => (qqq.answer))}</td>)
+  }
+}
 
 export default class Response extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      fieldsIsActive: [],
-
-      id: 0,
-      answers: [],
-      answer: "",
-      field: {
-        id: 0,
-        label: "",
-        type: "",
-        isRequired: false,
-        isActive: false,
-        options: []
-      }
+      fields: [],
+      response: [],
+      qwq: []
     }
-
-
-    this.addResponse = this.addResponse.bind(this)
-    this.cancel = this.cancel.bind(this)
-    this.view = this.view.bind(this)
   }
 
   componentDidMount() {
-    FieldService.getFieldsIsActive().then((result) => {
-      this.setState({ fieldsIsActive: result.data });
+    ResponseService.getResponses().then((result) => {
+      this.setState({ response: result.data.content });
+      FieldService.getAllFields().then((result) => {
+        this.setState({ fields: result.data });
+      });
     });
-  }
 
-  addResponse() {
   }
-
-  view() {
-    console.log(this.state.fieldsIsActive)
-  }
-
-  cancel() {/* 
-    this.setState({ id: 0 })
-    this.setState({ label: "" })
-    this.setState({ type: "" })
-    this.setState({ options: [] })
-    this.setState({ isRequired: false })
-    this.setState({ isActive: false })//update */
-  };
 
   render() {
     return (
-      <div>
-        <div>Response</div>
-       {/*  <SockJsClient
-          url={'http://localhost:8080/gs-guide-websocket'}
-          topics={['/topic/message']}
-          onConnect={console.log("Connected!!!!")}
-          onDisconnect={console.log("Disconnected!")}
-          onMessage={msg => {this.setState({ fieldsIsActive: msg },
-            console.log(msg))}}
-          debug={false}
-          /> */}
-        {this.state.fieldsIsActive.map((field) => (
-          <div>
-            <br></br><br></br>
-            <label>Label {field.label} </label>
-            <br></br>
-            <label>type {field.type} </label>
-            <br></br>
-            <label>isRequired {field.isRequired} </label>
-            <br></br>
-            <label>isActive {field.isActive} </label>
+      <div className="main">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <table className="table table-striped table-hover">
+                <caption>
+                  <div>
+                    <b>Responses</b>
+                  </div>
+                </caption>
+                <thead>
+                  <tr>
+                    {this.state.fields.map((field) => (
+                      <th> {field.label}
+                      </th>
+                    ))
+                    }
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.response.map((resp) => (
+                    <tr key={resp.id}>
+                      {this.state.fields.map((fields) => (
+                        <Find fields={fields} answers={resp.answers} />
+                      ))
+                      }
+                    </tr>
+                  ))
+                  }
+                </tbody>
+              </table>
+            </div>
           </div>
-        ))}
-
-      </div>
-
+        </div>
+      </div >
     )
   }
 }

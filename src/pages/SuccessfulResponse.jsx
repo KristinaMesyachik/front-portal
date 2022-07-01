1f92863e121c86a8757763ad67497734d50ac355
KristinaMesyachik <mesyachik2001@gmail.com>
2022-07-02 00:12:47 +0300

    Add successful page

---
+++ src/pages/SuccessfulResponse.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SuccessfulResponse() {
+
+    return (
+        <div>
+            <h3>Thank you!</h3>
+            <h4>Your response was saved.</h4>
+        </div>
+    )
+}
+
+export default SuccessfulResponse;
